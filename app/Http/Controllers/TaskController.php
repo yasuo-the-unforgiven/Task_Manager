484ef88bea4c61cc21b201e44d688c759fc164e1
yasuo <hassagim7@outlook.com>
2024-09-19 17:20:44 +0300

    init

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = QueryBuilder::for(Task::class)
+            ->allowedFilters('is_done')
+            ->paginate();
+        return new TaskCollection($tasks);
+    }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $validated = $request->validated();
+
+        $task = Task::create($validated);
+
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return new TaskResource($task);
+    }
+
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
